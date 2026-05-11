7dec3270559cf06c1cb95051817fc2f2c629d444
David Castells-Rufas <david.castells@uab.cat>
2026-05-11 16:32:37 +0200

    comments

--- COMP.sv
+++ COMP.sv
@@ -1,12 +1,13 @@
 module COMP (input logic [31:0] i1, i2, output logic [31:0] unsigned_lt, output logic [31:0] signed_lt, output logic lt, gt, eq);
+	// @todo check if lt, gt, eq should treat i1, i2 as signed or unsigned
+	
+	// Compare the two inputs
+	assign lt = (i1 <  i2);
+	assign gt = (i1 >  i2);
+	assign eq = (i1 == i2);
 
- // Compare the two inputs
- assign lt = (i1 <  i2);
- assign gt = (i1 >  i2);
- assign eq = (i1 == i2);
-
- 
- assign unsigned_lt  = (i1 < i2) ? 32'b1 : 32'b0 ;
- assign signed_lt = ($signed(i1) < $signed(i2)) ? 32'b1 : 32'b0;
+	 
+	assign unsigned_lt  = (i1 < i2) ? 32'b1 : 32'b0 ;
+	assign signed_lt = ($signed(i1) < $signed(i2)) ? 32'b1 : 32'b0;
  
 endmodule
