e7e339c09ac4837830743de977a91636f6782634
David Castells-Rufas <david.castells@uab.cat>
2026-05-15 11:56:25 +0200

    BUGFIX: S type immediate values

--- py4hw/immGen.py
+++ py4hw/immGen.py
@@ -1,13 +1,13 @@
 import py4hw
 
 class SelectType(py4hw.Logic):
-    def __init__(self, parent, name, opcode, imm_typ):
+    def __init__(self, parent, name, opcode, imm_type):
         super().__init__(parent, name)
 
         assert(opcode.getWidth() == 7)
         
         self.opcode = self.addIn ('opcode',  opcode)
-        self.imm_type = self.addOut('imm_typ', imm_typ)
+        self.imm_type = self.addOut('imm_type', imm_type)
 
     def propagate(self):
         a = (self.opcode.get() >> 4)  & ((1<<3)-1);
@@ -26,7 +26,8 @@ class SelectType(py4hw.Logic):
             elif (b==7): self.imm_type.put(0)
             elif (b==0xF): self.imm_type.put(4)
             else: self.imm_type.put(7)                
-
+        else:
+            self.imm_type.put(7)                
 
 class immGen(py4hw.Logic):
     def __init__(self, parent, name, ir, imm):
@@ -61,12 +62,12 @@ class immGen(py4hw.Logic):
         ir_30_21 = hlp.hw_range(ir, 30, 21)
         ir_20 = hlp.hw_bit(ir, 20)
         ir_19_12 = hlp.hw_range(ir, 19, 12)
-        ir_17_7 = hlp.hw_range(ir, 17, 7)
         ir_11_8 = hlp.hw_range(ir, 11, 8)
+        ir_11_7 = hlp.hw_range(ir, 11, 7)
         ir_7 = hlp.hw_bit(ir, 7)
 
         py4hw.SignExtend(self, 'imm_I', ir_31_20, imm_I)
-        py4hw.SignExtend(self, 'imm_S', hlp.hw_concatenate_msbf([ir_31_25, ir_17_7]), imm_S)
+        py4hw.SignExtend(self, 'imm_S', hlp.hw_concatenate_msbf([ir_31_25, ir_11_7]), imm_S)
         py4hw.SignExtend(self, 'imm_B', hlp.hw_concatenate_msbf([ir_31, ir_7, ir_30_25, ir_11_8, zero_1]), imm_B)
         py4hw.ConcatenateMSBF(self, 'imm_U', [ ir_31_12, zero_12], imm_U)
         py4hw.SignExtend(self, 'imm_J', hlp.hw_concatenate_msbf([ir_31, ir_19_12, ir_20, ir_30_21, zero_1]), imm_J)
