411527b5df158a8549ecdbf251ace1bb8c58ab23
David Castells-Rufas <david.castells@uab.cat>
2026-05-18 07:59:09 +0200

    avoid implicit variable declaration

--- SystemVerilog/SHIFT.sv
+++ SystemVerilog/SHIFT.sv
@@ -1,10 +1,12 @@
+`default_nettype none
+
 module SHIFT(input logic [31:0] i1, input logic [5:0] i2, input logic sign_extend, output logic [31:0] sr, sra, sl );
 
 logic [4:0] shamt;
-logic       arith;
+//logic       arith;
 
 assign shamt = i2[4:0];
-assign arith = i2[5];
+//assign arith = i2[5];
 
     always_comb begin
 		// Arithmetic Right Shift (SRA / SRAI) - Preserves sign bit
