f116cf94943b5916587445bdaae758a4eb8c9f39
David Castells-Rufas <david.castells@uab.cat>
2026-05-18 07:59:05 +0200

    avoid implicit variable declaration

--- SystemVerilog/RegFile.sv
+++ SystemVerilog/RegFile.sv
@@ -1,18 +1,28 @@
-module RegFile(input logic clk, input logic reset, input logic [4:0] rs1_addr, rs2_addr, rd_addr, input logic ld_rd, input logic [31:0] rd, output logic [31:0] rs1, rs2);
+`default_nettype none
 
-logic [31:0] q[0:31];
+module RegFile(input logic clk, input logic reset, 
+	input logic [4:0] rs1_addr, rs2_addr, rd_addr, 
+	input logic ld_rd, input logic [31:0] rd, 
+	output logic [31:0] rs1, rs2,
+	// Debug wires
+	output logic [31:0] x10
+	);
 
-assign q[0] = 32'b0;
+	logic [31:0] q[0:31];
 
-genvar i;
- generate
-	  for (i = 1; i < 32; i++) begin : bit_slice
-			
-			Reg32 r(.clk(clk), .reset(reset), .load(ld_rd && (rd_addr == i)), .d(rd), .q(q[i]));
-	  end
-endgenerate
+	assign q[0] = 32'b0;
 
-assign rs1 = q[rs1_addr];
-assign rs2 = q[rs2_addr];
+	genvar i;
+	 generate
+		  for (i = 1; i < 32; i++) begin : bit_slice
+				
+				Reg32 r(.clk(clk), .reset(reset), .load(ld_rd && (rd_addr == i)), .d(rd), .q(q[i]));
+		  end
+	endgenerate
+
+	assign rs1 = q[rs1_addr];
+	assign rs2 = q[rs2_addr];
+
+	assign x10 = q[10];
 
 endmodule
